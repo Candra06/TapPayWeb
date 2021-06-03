988dc89e90edabbbf78dbec6adca53b6e7f55af6
candrabiyu4 <candrabiyu4@gmail.com>
2021-06-03 14:01:40 +0700

    tagihan

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/linkstorage', function () {
+    Artisan::call('storage:link');
+});
+
 Route::get('/', function () {
     return view('home.login');
 });
