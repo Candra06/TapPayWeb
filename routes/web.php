37eb66547f1b063ae4fa1d4fc7f08911e586c043
candrabiyu4 <candrabiyu4@gmail.com>
2021-06-03 14:02:03 +0700

    tagihan

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\API\TagihanController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 /*
 |--------------------------------------------------------------------------
